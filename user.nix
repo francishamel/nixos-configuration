3f6083598b70dadbe8895cd5da078b4e8b34cf37
Francis Hamel <francishamel96@gmail.com>
2020-11-19 15:54:19 -0500

    Revert "Remove oh-my-zsh"
    This reverts commit 32d8095b26aa62f7c9ad1df4a0ab32e1322fb15a.

--- user.nix
+++ user.nix
@@ -197,18 +197,7 @@
 
         enable = true;
 
-        initExtra = ''
-          bindkey "^[[1;5C" forward-word
-          bindkey "^[[1;5D" backward-word
-
-          # Enable up/down arrow search
-          autoload -U up-line-or-beginning-search
-          autoload -U down-line-or-beginning-search
-          zle -N up-line-or-beginning-search
-          zle -N down-line-or-beginning-search
-          bindkey "^[[A" up-line-or-beginning-search # Up
-          bindkey "^[[B" down-line-or-beginning-search # Down
-        '';
+        oh-my-zsh.enable = true;
 
         shellAliases = {
           ls = "lsd";
