32d8095b26aa62f7c9ad1df4a0ab32e1322fb15a
Francis Hamel <francishamel96@gmail.com>
2020-11-19 14:25:10 -0500

    Remove oh-my-zsh
    I was just using it for the bindkey

--- user.nix
+++ user.nix
@@ -195,7 +195,18 @@
 
         enable = true;
 
-        oh-my-zsh.enable = true;
+        initExtra = ''
+          bindkey "^[[1;5C" forward-word
+          bindkey "^[[1;5D" backward-word
+
+          # Enable up/down arrow search
+          autoload -U up-line-or-beginning-search
+          autoload -U down-line-or-beginning-search
+          zle -N up-line-or-beginning-search
+          zle -N down-line-or-beginning-search
+          bindkey "^[[A" up-line-or-beginning-search # Up
+          bindkey "^[[B" down-line-or-beginning-search # Down
+        '';
 
         shellAliases = {
           ls = "ls --color=auto";
